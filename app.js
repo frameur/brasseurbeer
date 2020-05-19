16051d16eb3e90063aed242ff69586980bd3d68c
frameur <francoisramos6121@gmail.com>
2020-05-19 13:26:32 +0200

    ajout index

--- app.js
+++ app.js
@@ -1,23 +1,62 @@
 const express = require('express')
-const mongoose = require('mongoose')
+    , mongoose = require('mongoose')
+    , multer = require('multer')
+    , app = express()
+    , port = 2002
 
 
 mongoose.connect('mongodb://localhost:27017/brasbeer',{useNewUrlParser: true,
 useUnifiedTopology: true});
 
-const app = express();
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
+// Route
+const { getHomePage} = require('./routes/index')
+
 
 // Express Static
 app.use(express.static("public"));
+
+// Upload image
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const date = Date.now();
+    cb(null, date + "-" + file.originalname);
+    
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 8 * 2048 * 2048,
+    files: 1,
+  },
+  fileFilter: function (req, file, cb) {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/gif"  ||
+      file.mimetype === "image/webp"
+    ) {
+      cb(null, true);
+    } else
+      cb(new Error("Le fichier doit être au format png, jpg, jpeg ou gif."));
+  },
+});
 
 // Template EJS
 app.set("view engine", "ejs");
 
 //Page d'accueil
-app.get('/', (req,res) =>{
-  res.render("index")  
-   
-})
+app.get('/', getHomePage)
 
 //Page fabrication biere
 app.get('/fabbeer', (req,res) =>{
@@ -64,7 +103,7 @@ app.get ("/contact", (req,res) => {
 
 
 // Localhost
-const port = 2002;
+
 app.listen(port, () => {
   console.log(process.env.PORT ||
     `Ecoute le port ${port}, lancé à : ${new Date().toLocaleString()}`
